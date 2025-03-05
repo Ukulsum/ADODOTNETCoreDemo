0ad7155a976f21818d3aa6fb799b7be59986889c
ukulsum <ukulsum1280@gmail.com>
2025-03-06 04:13:41 +0600

    a

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -14,27 +14,51 @@ namespace ADODOTNETCoreDemo
                 //I am using Windows Authentication and hence no need to pass the User Id and Password
                 string connectionString = "Server=DESKTOP-M6B26UP\\SQLEXPRESS;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
 
+                //Example student data to insert
+                string studentFirstName = "Umme";
+                string studentLastName = "Kulsum";
+                string studentEmail = "uku125@gmail.com";
+              
+                //SQL Insert statement
+                string insertSql = "Insert INTO Students(FirstName,LastName,Email) Values (@FirstName,@LastName, @Email)";
+
                 //While Creating the SqlConnection passing the Connection String
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     //Open the Connection
                     connection.Open();
 
-                    //Create the Command Text
-                    string createTableCommandText = @"
-                    CREATE TABLE Students (
-                        Id INT PRIMARY KEY IDENTITY(1,1),
-                        FirstName NVARCHAR(50),
-                        LastName NVARCHAR(50),
-                        Email NVARCHAR(50)
-                    );";
+                    ////Create the Command Text
+                    //string createTableCommandText = @"
+                    //CREATE TABLE Students (
+                    //    Id INT PRIMARY KEY IDENTITY(1,1),
+                    //    FirstName NVARCHAR(50),
+                    //    LastName NVARCHAR(50),
+                    //    Email NVARCHAR(50)
+                    //);";
 
                     //Create an instance of the SqlCommand object by using the Command text and Connection object
-                    using (SqlCommand command = new SqlCommand(createTableCommandText, connection))
+                    ////using (SqlCommand command = new SqlCommand(createTableCommandText, connection))
+                    using (SqlCommand command = new SqlCommand(insertSql, connection))
                     {
+
+                        //Add parameters to prevent SQL injection
+                        command.Parameters.AddWithValue("@FirstName", studentFirstName);
+                        command.Parameters.AddWithValue("@LastName", studentLastName);
+                        command.Parameters.AddWithValue("@Email", studentEmail);
                         //Call ExecuteNonQuery Method to Execute the command in the Provided Connection
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("Table 'Students' Created Successfully.");
+                        //Execute the Command
+                        int result = command.ExecuteNonQuery();
+                        //Check the result
+                        if(result < 0)
+                        {
+                            Console.WriteLine("Error Inserting Data Into Database!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Data Inserted Successfully.");
+                        }
+                        
                     }
 
                     //Close the Connection
